2b19f0779196d466fa0fc536bbcc4d77fa9005e1
DrZder <bournonoville2@me.com>
2021-07-20 02:05:43 +0200

    first c

---
+++ printfman.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <direct.h>
+
+int main(){
+//main printout pretty intuitive lol;
+printf(" _____________________________________________\n");
+printf(" |  DATA TYPE  | UNDER TYPE  |  FORMAT SPEC  |\n");
+printf(" |-------------|-------------|---------------|\n");
+printf(" |             |short signed |    %%d or %%I   |\n");
+printf(" |             |-------------|---------------|\n");
+printf(" |             |short usigned|      %%u       |\n");
+printf(" |             |-------------|---------------|\n");
+printf(" |  integers   | long signed |      %%ld      |\n");
+printf(" |             |-------------|---------------|\n");
+printf(" |             |long unsigned|      %%lu      |\n");
+printf(" |             |-------------|---------------|\n");
+printf(" |             |usingned hex |	    %%x       |\n");
+printf(" |             |-------------|---------------|\n");
+printf(" |             |usigned octal|      %%o       |\n");
+printf(" |-------------|-------------|---------------|\n");
+printf(" |             |   float     |      %%f       |\n");
+printf(" |    real     |-------------|---------------|\n");
+printf(" |             |   double    |      %%lf      |\n");
+printf(" |-------------|-------------|---------------|\n");
+printf(" |  character  |    all      |      %%c       |\n");
+printf(" |-------------|-------------|---------------|\n");
+printf(" |   string    |    all      |      %%s       |\n");
+printf(" |_____________|_____________|_______________|");
+sleep(1000);
+return 0;
+}
